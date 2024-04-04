75b53291f42dee33c09a0c2f7141d8381c830937
Thedkji <nmquang2303@gmail.com>
2024-04-04 18:13:34 +0700

    hoan thien xoa san pham

--- controllers/Sanpham/SanphamAddController.js
+++ controllers/Sanpham/SanphamAddController.js
@@ -1,72 +1,95 @@
-window.SanphamAddController = function($scope, $http) {
-    let apiSt = "http://localhost:3000/Status";
-    let apiSP ="http://localhost:3000/sanpham"
-    let apiDM ="http://localhost:3000/danhmuc"
+window.SanphamAddController = function ($scope, $http, $location) {
+  let apiSt = "http://localhost:3000/Status";
+  let apiSP = "http://localhost:3000/sanpham";
+  let apiDM = "http://localhost:3000/danhmuc";
 
-    $scope.getStatus = function() {
-        $http.get(apiSt).then(function(res) {
-            $scope.status = res.data;
-        })
-    }
-    $scope.getStatus();
+  $scope.getStatus = function () {
+    $http.get(apiSt).then(function (res) {
+      $scope.status = res.data;
+    });
+  };
+  $scope.getStatus();
 
-    $scope.getDanhMuc = function() {
-        $http.get(apiDM).then(function(res) {
-            $scope.danhmuc = res.data;
-        })
-    }
-    $scope.getDanhMuc();
+  $scope.getDanhMuc = function () {
+    $http.get(apiDM).then(function (res) {
+      $scope.danhmuc = res.data;
+    });
+  };
+  $scope.getDanhMuc();
 
-    $http.get(apiSP).then(function(res) {
-        $scope.btn_ThemSP = function() {
-            let flag = false;
-    
-            $scope.err = {
-                NameEmpty: false,
-                NameLength: false,
-                NameNotNumber: false,
-                NameDuplication: false,
-                PriceEmpty: false,
-                PriceInteger: false,
-                DanhMucEmpty: false,
-                StatusEmpty: false
-            };
-    
-            if (!$scope.valueSP || !$scope.valueSP.Name) {
-                $scope.err.NameEmpty = true;
-                flag = true;
-            }  else if(!/^[a-zA-z\p{L}\s]{2}[a-zA-z0-9\p{L}\s]+$/u.test($scope.valueSP.Name)){
-                $scope.err.NameNotNumber =  true;
-                flag=true;
-            }
-            else if ($scope.valueSP.Name.length < 3) {
-                $scope.err.NameLength = true;
-                flag = true;
-            }
-            else if(res.data.some((item)=>item.name == $scope.valueSP.Name)){
-                $scope.err.NameDuplication = true;
-                flag = true;
-            }
-            
-            if (!$scope.valueSP || !$scope.valueSP.Price) {
-                $scope.err.PriceEmpty = true;
-                flag = true;
-            }
-            else if($scope.valueSP.Price < 0){
-                $scope.err.PriceInteger = true;
-                flag = true;
-            }
+  $http.get(apiSP).then(function (res) {
+    $scope.btn_ThemSP = function () {
+      let flag = false;
 
-            if (!$scope.valueSP || !$scope.valueSP.DanhMuc) {
-                $scope.err.DanhMucEmpty = true;
-                flag = true;
-            }  
-    
-            if (!$scope.valueSP || !$scope.valueSP.Status) {
-                $scope.err.StatusEmpty = true;
-                flag = true;
-            }  
-            
-        }
-    })
-}
+      $scope.err = {
+        NameEmpty: false,
+        NameLength: false,
+        NameNotNumber: false,
+        NameDuplication: false,
+        PriceEmpty: false,
+        PriceInteger: false,
+        DanhMucEmpty: false,
+        StatusEmpty: false,
+      };
+
+      if (!$scope.valueSP || !$scope.valueSP.name) {
+        $scope.err.NameEmpty = true;
+        flag = true;
+      } else if (
+        !/^[a-zA-z\p{L}\s]{2}[a-zA-z0-9\p{L}\s]+$/u.test($scope.valueSP.name)
+      ) {
+        $scope.err.NameNotNumber = true;
+        flag = true;
+      } else if ($scope.valueSP.name.length < 4) {
+        $scope.err.NameLength = true;
+        flag = true;
+      } else if (res.data.some((item) => item.name == $scope.valueSP.name)) {
+        $scope.err.NameDuplication = true;
+        flag = true;
+      }
+
+      if (!$scope.valueSP || !$scope.valueSP.price) {
+        $scope.err.PriceEmpty = true;
+        flag = true;
+      } else if ($scope.valueSP.price < 0) {
+        $scope.err.PriceInteger = true;
+        flag = true;
+      }
+
+      if (!$scope.valueSP || !$scope.valueSP.id_danhmuc) {
+        $scope.err.DanhMucEmpty = true;
+        flag = true;
+      }
+
+      if (!$scope.valueSP || !$scope.valueSP.id_status) {
+        $scope.err.StatusEmpty = true;
+        flag = true;
+      }
+
+      if (!flag) {
+        let date = new Date();
+        let new_Item = {
+          ...$scope.valueSP,
+          create_at:
+            date.getDate() +
+            "/" +
+            (date.getMonth() + 1) +
+            "/" +
+            date.getFullYear() +
+            " " +
+            date.getHours() +
+            ":" +
+            date.getMinutes() 
+        };
+
+        $http.post(apiSP, new_Item).then(function (res) {
+          if (res.status == 201) {
+            alert("Thêm sản phẩm thành công");
+            $location.path("/sanpham/list");
+          }
+        });
+      }
+    };
+
+  });
+};
--- controllers/Sanpham/SanphamListController.js
+++ controllers/Sanpham/SanphamListController.js
@@ -1,27 +1,38 @@
-window.SanphamListController = function($scope,$http,$location) {
-    let api = "http://localhost:3000/sanpham";
-    let apiDM = "http://localhost:3000/danhmuc";
-    let apiSt = "http://localhost:3000/Status";
-    
-    $scope.getDaTa = function() {
-        $http.get(api).then(function(res) {
-            $scope.products = res.data;
-            console.log($scope.pro)
-        })
-    }
-    $scope.getDaTa()
+window.SanphamListController = function ($scope, $http, $location) {
+  let api = "http://localhost:3000/sanpham";
+  let apiDM = "http://localhost:3000/danhmuc";
+  let apiSt = "http://localhost:3000/Status";
 
-    $scope.getDanhmuc = function() {
-        $http.get(apiDM).then(function(res) {
-            $scope.danhmuc = res.data;
-        })
-    }
-    $scope.getDanhmuc();
+  $scope.getDaTa = function () {
+    $http.get(api).then(function (res) {
+      $scope.products = res.data;
+      console.log($scope.pro);
+    });
+  };
+  $scope.getDaTa();
 
-    $scope.getStatus = function() {
-        $http.get(apiSt).then(function(res) {
-            $scope.status = res.data;
-        })
+  $scope.getDanhmuc = function () {
+    $http.get(apiDM).then(function (res) {
+      $scope.danhmuc = res.data;
+    });
+  };
+  $scope.getDanhmuc();
+
+  $scope.getStatus = function () {
+    $http.get(apiSt).then(function (res) {
+      if (res.status == 200) {
+        $scope.status = res.data;
+      }
+    });
+  };
+  $scope.getStatus();
+
+  $scope.onDelete = function (id) {
+    if(confirm("Bạn có chắc chắn muốn xóa ko?")){
+        $http.delete(`${api}/${id}`).then(function (res) {
+          alert("Xóa thành công");
+          $scope.getDaTa();
+        });
     }
-    $scope.getStatus();
-}
+  };
+};
